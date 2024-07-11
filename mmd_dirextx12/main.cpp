f20bb2dfa9d32ca7acf64bc2d0cbb660617a3b9d
Saigo1997 <a.saigo1997@gmail.com>
2024-07-11 23:20:06 +0900

    Direct3Dの初期化

--- mmd_dirextx12/main.cpp
+++ mmd_dirextx12/main.cpp
@@ -1,6 +1,15 @@
 #include <windows.h>
 #include <tchar.h>
 
+#include <string>
+#include <vector>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
 #define WINDOW_WIDTH    800
 #define WINDOW_HEIGHT   600
 HINSTANCE hInst;
@@ -35,12 +44,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     hInst = hInstance;
 
+    RECT wrc = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
+    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
     HWND hWnd = CreateWindow(
         szWindowClass,
         szTitle,
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT,
-        WINDOW_WIDTH, WINDOW_HEIGHT,
+        wrc.right - wrc.left,
+        wrc.bottom - wrc.top,
         NULL,
         NULL,
         hInstance,
@@ -51,6 +64,53 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     {
         MessageBox(NULL, _T("ウィンドウ生成に失敗しました!"), szWindowClass, NULL);
         return -1;
+    }
+
+    ID3D12Device* _dev = nullptr;
+    IDXGIFactory6* _dxgiFactory = nullptr;
+    // IDXGISwapchain4* _swapchain = nullptr;
+
+    // アダプタを探す
+    auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+    if (!_dxgiFactory) {
+		MessageBox(NULL, _T("CreateDXGIFactory1の生成に失敗しました"), szWindowClass, NULL);
+		return -1;
+	}
+
+    std::vector<IDXGIAdapter*> adaptors;
+    IDXGIAdapter* adaptor = nullptr;
+    for (int i = 0; _dxgiFactory->EnumAdapters(i, &adaptor) != DXGI_ERROR_NOT_FOUND; i++) {
+        adaptors.push_back(adaptor);
+    }
+
+    for (auto ad : adaptors) {
+        DXGI_ADAPTER_DESC desc = {};
+		ad->GetDesc(&desc);
+		
+        std::wstring strDesc = desc.Description;
+        if (strDesc.find(L"NVIDIA") != std::string::npos) {
+            adaptor = ad;
+            break;
+        }
+	}
+
+    D3D_FEATURE_LEVEL levels[] = {
+        D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+    };
+    D3D_FEATURE_LEVEL featureLevel;
+    for (auto lv : levels) {
+        if (D3D12CreateDevice(adaptor, lv, IID_PPV_ARGS(&_dev)) == S_OK) {
+            featureLevel = lv;
+			break;
+        }
+    }
+
+    if (!_dev) {
+        MessageBox(NULL, _T("Direct3D12デバイスの生成に失敗しました"), szWindowClass, NULL);
+		return -1;
     }
 
     ShowWindow(hWnd, nCmdShow);
